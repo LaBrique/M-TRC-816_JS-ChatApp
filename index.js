745976bde1211f9d009e28de8a6bb446d8054739
LaBrique <corentin.marzin@epitech.eu>
2021-04-23 17:36:33 +0200

    added basic room management

--- index.js
+++ index.js
@@ -4,15 +4,29 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+	socket.on('join room', (msg) => {
+		console.log(socket.rooms);
+		socket.rooms.forEach((item) => {
+			if (item == socket.id) return;
+			socket.leave(item);
+		});
+		socket.join(msg.room);
+		console.log(socket.rooms);
+	})
+	socket.on('chat message', (msg) => {
+		console.log(msg);
+		io.to(msg.room).emit('chat message', msg.msg);
+	});
 });
 
+app.get('/clients', (req, res) => {
+	res.send(String(io.sockets.sockets.size));
+})
+
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
