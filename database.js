c1581930b8f103dace312172c19d9c54486db413
LaBrique <corentin.marzin@epitech.eu>
2021-04-28 16:27:04 +0200

    Added message saving

--- database.js
+++ database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 // connect to Mongo daemon
 mongoose.connect(
-    'mongodb://mongo:27017/expressmongo',
-    // 'mongodb+srv://DevVRGhost:grossevache1&@cluster0.faeme.azure.mongodb.net/vrghost_db?retryWrites=true&w=majority',
+    // 'mongodb://mongo:27017/expressmongo',
+    'mongodb+srv://DevVRGhost:grossevache1&@cluster0.faeme.azure.mongodb.net/vrghost_db?retryWrites=true&w=majority',
     { useNewUrlParser: true },
 )
 .catch(err => console.log(err))
--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const mongoose = require('./database.js');
+const UserModel = require("./models/jusersModel.js");
+const RoomModel = require("./models/roomModel.js")
+
+function saveMessage(msg) {
+	UserModel.findOne({_id: msg.sender}, (err, user) => {
+		if (user) {
+			RoomModel.findOne({_id: msg.room}, (err, room) => {
+				room.messages.push({
+					date: new Date,
+					content: msg.msg,
+					sender: user.surname,
+					readby: [user._id]
+				});
+				room.save();
+			})
+		}
+	})
+}
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
@@ -19,12 +38,79 @@ io.on('connection', (socket) => {
 	})
 	socket.on('chat message', (msg) => {
 		console.log(msg);
+		saveMessage(msg);
 		io.to(msg.room).emit('chat message', msg.msg);
 	});
 });
 
+app.post('/user', (req, res) => {
+	const surname = req.query.surname;
+	const email = req.query.email;
+	const password = req.query.password;
+
+	var create = new UserModel({
+		surname: surname,
+		email: email,
+		password: password,
+		rooms: []
+	});
+	create.save().then((report) => {
+		res.send(200);
+	})
+	.catch((err) => {
+		console.log(err);
+		res.send(301);
+	});
+
+})
+
+app.post('/rooms/:userId', (req, res) => {
+	const userId = req.params.userId;
+
+	const surname = req.query.surname;
+
+	UserModel.findOne({'_id': userId}, (err, user) => {
+		var create = new RoomModel({
+			surname: surname,
+			owner: userId,
+			invited: [],
+			messages:  [{
+		            date: new Date,
+		            content: `Room created by ${user.surname}.`,
+		            sender: "System",
+		            readby: []
+		        }]
+		});
+		create.save().then((room) => {
+			user.rooms.push(room._id);
+			user.save();
+			res.send(200);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.send(301);
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+		res.send(301);
+	})
+})
+
 app.get('/clients', (req, res) => {
 	res.send(String(io.sockets.sockets.size));
+})
+
+app.get('/rooms/:userId', (req, res) => {
+	const userId = req.params.userId;
+
+	UserModel.findOne({'_id': userId}, (err, user) => {
+		if (!user) {
+			res.send(404);
+		} else {
+			res.send(user.rooms);
+		}
+	})
 })
 
 http.listen(3000, () => {
