d09ff19cfefc3cf8f54c5240f7a94d48d5ba8fd6
Benoitydlb <byverdelabrucholerie@findweek.fr>
2021-05-14 15:16:30 +0100

    modif

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const app = require('express')();
+var express = require('express');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const nodemailer = require('nodemailer');
 const mongoose = require('./database.js');
-const UserModel = require("./models/jusersModel.js");
+const juserModel = require("./models/jusersModel.js");
 const RoomModel = require("./models/roomModel.js")
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 function saveMessage(msg) {
-	UserModel.findOne({_id: msg.sender}, (err, user) => {
+	juserModel.findOne({_id: msg.sender}, (err, user) => {
 		if (user) {
 			RoomModel.findOne({_id: msg.room}, (err, room) => {
 				room.messages.push({
@@ -20,6 +25,49 @@ function saveMessage(msg) {
 			})
 		}
 	})
+}
+
+function sendmail(user) {
+	var transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: "vrghost.eip@gmail.com",
+			pass: "grossevache1"
+		}
+	});
+
+	var mailOptions = {
+		from: 'VRghost.project@gmail.com',
+		to: user._id,
+		subject: 'Activation de votre compte Vchat',
+		text: `Cliquez sur le lien pour activer votre compte:\nhttp://localhost:8080/api/users-creates/${user._id}/validation`,
+		html: `
+			<div style="text-align:center; background-color:#4d4d4d; padding-bottom:30px; color:#ffffff;margin-top: -4px;height: 123px;width: 816px;margin-left: 2px;border-top-width: 2px;border-top-style: solid; border-color: black">
+				${user.surname}, bienvenue chez Vchat!
+				<br/>
+				<br/>
+				Afin de finaliser la création de votre compte, cliquez sur le bouton "Activer", ou utilisez ce lien:
+				<a href="http://localhost:8080/api/users-creates/${user._id}/validation" style="color: #a4a4a4">
+					http://localhost:8080/api/users-creates/${user._id}/validation
+				</a>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<a href="http://localhost:8080/api/users-creates/${user._id}/validation" style="background-color: #38d39f; padding: 10px 15px; border: 2px; border-color: #ffffff; border-style: solid; color: #4d4d4d; font-size: 22px; text-decoration: none">
+					Activer
+				</a>
+			</div>
+		`
+	}
+
+	transporter.sendMail(mailOptions, (err, info) => {
+		if (err)
+			console.log(err);
+		else
+			console.log(info.response);
+	});
+	transporter.close();
 }
 
 app.get('/', (req, res) => {
@@ -43,19 +91,22 @@ io.on('connection', (socket) => {
 	});
 });
 
-app.post('/user', (req, res) => {
+app.post('/users', (req, res) => {
+	console.log(req.body)
 	const surname = req.query.surname;
 	const email = req.query.email;
 	const password = req.query.password;
 
-	var create = new UserModel({
+	var create = new juserModel({
 		surname: surname,
 		email: email,
 		password: password,
-		rooms: []
+		rooms: [],
+		confirmed: false,
+		login: false
 	});
 	create.save().then((report) => {
-		res.send(200);
+		res.sendStatus(200);
 	})
 	.catch((err) => {
 		console.log(err);
@@ -64,12 +115,92 @@ app.post('/user', (req, res) => {
 
 })
 
+app.get('/users', (req, res) => {
+	juserModel.find({}, (err, users) => {
+		res.send({'users': users});
+	}).catch((err) => {
+		console.log(err);
+		res.send(301);
+	})
+})
+
+app.get('/users/:id/confirmation', (req, res) => {
+	const userId = req.params.id;
+	juserModel.findOne({'_id': userId}, (err, user) => {
+		user.confirmed = true;
+		user.save().then((user) => {
+			res.redirect('');
+		}).catch((err) => {
+			console.log(err);
+			res.send(301);
+		});
+	}).catch((err) => {
+		console.log(err);
+		res.send(301);
+	})
+})
+
+
+app.get('/users/:id/logout', (req, res) => {
+	const userId = req.params.id;
+	juserModel.findOne({'_id': userId}, (err, user) => {
+		user.login = false;
+		user.save().then((user) => {
+			res.send(204);
+		}).catch((err) => {
+			console.log(err);
+			res.send(301);
+		});
+	}).catch((err) => {
+		console.log(err);
+		res.send(301);
+	})
+})
+
+app.post('/users/:id/password', (req, res) => {
+	const userId = req.params.id;
+	const password = req.query.password
+	juserModel.findOne({'_id': userId}, (err, user) => {
+		user.password = password;
+		user.save().then((user) => {
+			res.send(204);
+		}).catch((err) => {
+			console.log(err);
+			res.send(301);
+		});
+	}).catch((err) => {
+		console.log(err);
+		res.send(301);
+	})
+})
+
+app.post('/login', (req, res) => {
+	const email = req.query.email;
+	const password = req.query.password;
+	juserModel.findOne({'email': email, 'password': password}, (err, user) => {
+		if (user.confirmed) {
+			user.login = true;
+			user.save().then((user) => {
+				res.send(user._id);
+			}).catch((err) => {
+				console.log(err);
+				res.send(301);
+			});
+		} else {
+			res.send(301);
+		}
+	}).catch((err) => {
+		console.log(err);
+		res.send(301);
+	})
+})
+
 app.post('/rooms/:userId', (req, res) => {
 	const userId = req.params.userId;
 
 	const surname = req.query.surname;
 
-	UserModel.findOne({'_id': userId}, (err, user) => {
+	juserModel.findOne({'_id': userId}, (err, user) => {
 		var create = new RoomModel({
 			surname: surname,
 			owner: userId,
@@ -104,7 +235,7 @@ app.get('/clients', (req, res) => {
 app.get('/rooms/:userId', (req, res) => {
 	const userId = req.params.userId;
 
-	UserModel.findOne({'_id': userId}, (err, user) => {
+	juserModel.findOne({'_id': userId}, (err, user) => {
 		if (!user) {
 			res.send(404);
 		} else {
--- models/jusersModel.js
+++ models/jusersModel.js
@@ -4,7 +4,9 @@ var juserSchema = new mongoose.Schema({
     surname: String,
     email: String,
     password: String,
-    rooms: [String]
+    rooms: [String],
+    login: Boolean,
+    confirmed: Boolean
 });
 
 var juserModel = mongoose.model('juser', juserSchema);
